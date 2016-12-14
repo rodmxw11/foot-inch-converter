5d1efbdc02a3c650af44e7ec61a7f8b01e80a35e
Rodney Maxwell <rodmxw11@gmail.com>
2016-12-14 01:12:13 -0500

    Able to parse float feet inches and fractions

--- src/foot_inch_converter/core.clj
+++ src/foot_inch_converter/core.clj
@@ -1,6 +1,23 @@
 (ns foot-inch-converter.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def foot-inch-regex
+;;  #"^\s*(\d+)(\.\d*)?(?:\s+(\d+)(\.\d*)?(?:\s+(\d+)\/(\d+))?)?\s*$"
+  #"(?x)^\s*
+  (\d+(?:\.\d*)?)
+  (?:\s+
+     (\d+(?:\.\d*)?)
+     (?:\s+(\d+)\/(\d+))?
+    )?"
+  )
+
+
+(defn parse-foot-inch [s]
+  (if-let [v (re-matches foot-inch-regex s)]
+    (->>
+      v
+      (drop 1)
+      (map #(if % (Float/parseFloat %)))
+      vec
+      )
+    )
+  )
--- test/foot_inch_converter/core_test.clj
+++ test/foot_inch_converter/core_test.clj
@@ -3,5 +3,29 @@
             [foot-inch-converter.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "foot-inch-regex basic test"
+    (is (re-matches foot-inch-regex "0"))
+    (are [s] (re-matches foot-inch-regex s)
+             "0"
+             "0."
+             "1.2"
+             "0 1"
+             "0.0 1.1"
+             "0 1.22"
+             "0 0 1/2"
+             "1.223 3.331 3/138"
+             )
+    ))
+
+(deftest b-test
+  (testing "foot-inch-regex digit parsing"
+    (is (= [0.0 nil nil nil] (parse-foot-inch "0")))
+    (are [s v] (= v (parse-foot-inch s))
+               "0" [0.0 nil nil nil]
+               "0." [0.0 nil nil nil]
+               "000.000" [0.0 nil nil nil]
+               "1 2" [1.0 2.0 nil nil]
+               "1.23 4.56 3/4" [1.23 4.56 3 4]
+               )
+    ))
+
