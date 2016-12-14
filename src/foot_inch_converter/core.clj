db5093be2bdb0d5d40588ac72a204da4129e5606
Rodney Maxwell <rodmxw11@gmail.com>
2016-12-14 01:58:11 -0500

    to-feet works OK

--- src/foot_inch_converter/core.clj
+++ src/foot_inch_converter/core.clj
@@ -10,6 +10,21 @@
     )?"
   )
 
+(def epsilon 0.0000001)
+
+(defn float= [f1 f2]
+  (case [(nil? f1) (nil? f2)]
+    ([true true]) true
+    ([true false] [false true]) false
+    (<= (Math/abs (- f1 f2)) epsilon)
+    )
+  )
+
+(defn vfloat= [v1 v2]
+  (every? true?
+          (map float= v1 v2)
+          )
+  )
 
 (defn parse-foot-inch [s]
   (if-let [v (re-matches foot-inch-regex s)]
@@ -21,3 +36,20 @@
       )
     )
   )
+
+(defn to-feet [[feet inches num denom]]
+  (+
+    feet
+    (if inches (/ inches 12.0) 0.0)
+    (if (and num denom (not (zero? denom))) (/ num denom 12.0) 0.0)
+    )
+  )
+
+(defn to-inches [[feet inches num denom]]
+  (+
+    (* feet 12.0)
+    (if inches inches 0.0)
+    (if (and num denom (not (zero? denom))) (/ num denom 1.0) 0.0)
+    )
+  )
+
--- test/foot_inch_converter/core_test.clj
+++ test/foot_inch_converter/core_test.clj
@@ -19,13 +19,24 @@
 
 (deftest b-test
   (testing "foot-inch-regex digit parsing"
-    (is (= [0.0 nil nil nil] (parse-foot-inch "0")))
-    (are [s v] (= v (parse-foot-inch s))
+    (is (vfloat= [0.0 nil nil nil] (parse-foot-inch "0")))
+    (are [s v] (vfloat= v (parse-foot-inch s))
                "0" [0.0 nil nil nil]
                "0." [0.0 nil nil nil]
                "000.000" [0.0 nil nil nil]
                "1 2" [1.0 2.0 nil nil]
-               "1.23 4.56 3/4" [1.23 4.56 3 4]
+               "1.23 4.56 3/4" [1.23 4.56 3.0 4.0]
                )
+    ))
+
+(deftest c-test
+  (testing "to-feet works OK"
+    (is (= 1.0 (to-feet [1.0 nil nil nil])))
+    (are [ft v] (= ft (to-feet v))
+                1.0 [1.0 nil nil nil]
+                1.5 [1.0 6.0 nil nil]
+                1.5 [1.0 0.0 12.0 2.0]
+                2.0 [1.0 6.0 12.0 2.0]
+                )
     ))
 
